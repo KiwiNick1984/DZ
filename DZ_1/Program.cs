8bad7cc008aa4635d2b7f250c13b7de4218f98d6
Kiwi <Kiwi@KIWIDESKTOP>
2023-05-31 18:55:32 +0300

    Add project files.

---
+++ DZ_1/Program.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_1
+{
+    internal class Program
+    {
+        //для ДЗ 2
+        static string[] Splitting(string inStr)
+        { 
+            string[] outMasStr;
+            string[] tempMasStr;
+            string tempStr;
+            if (inStr.IndexOf(',') > 0)
+            {
+                tempStr = inStr.Substring(0, inStr.IndexOf(','));
+                tempStr = tempStr.Trim();
+                tempMasStr = Splitting(inStr.Substring(inStr.IndexOf(',')+1));
+                outMasStr = new string[tempMasStr.Length + 1];
+                outMasStr[0] = string.Copy(tempStr);
+                for (int i = 0; i < tempMasStr.Length; i++)
+                {
+                    outMasStr[i + 1] = tempMasStr[i];
+                }
+            }
+            else 
+            {
+                outMasStr = new string[1];
+                outMasStr[0] = string.Copy(inStr.Trim());
+            }
+            return outMasStr;
+        }
+
+        //для ДЗ 3
+        static string IntToStr(int inInt, int i, string intStr)
+        {            
+            if (i == 0)
+            {
+                if (inInt == 0)
+                    intStr = ((IntDigit)0).ToString();
+                else if (inInt % 100 < 20)
+                    intStr = IntToStr(inInt / 100, 100, ((IntDigit)(inInt % 100)).ToString()) + " " + intStr;
+                else
+                    intStr = IntToStr(inInt / 10, 10, ((IntDigit)(inInt % 10)).ToString()) + " " + intStr;
+            }
+            else
+            {
+                if ((inInt % 10) > 0)
+                    intStr = IntToStr(inInt / 10, 10 * i, ((IntDigit)(inInt % 10 * i)).ToString() + " " + intStr);
+            }
+            return intStr;
+        }
+        enum IntDigit
+        {
+            Ноль,
+            один,
+            два,
+            три,
+            четыре,
+            пять,
+            шесть,
+            семь,
+            восемь,
+            девять,
+            десять,
+            одиннадцать,
+            двенадцать,
+            тринадцать,
+            четырнадцать,
+            пятнадцать,
+            шестнадцать,
+            семнадцать,
+            восемннадцать,
+            девятнадцать,
+            двадцать = 20,
+            тридцать = 30,
+            сорок = 40,
+            пятьдесят = 50,
+            шестьдесят = 60,
+            семьдесят = 70,
+            восемьдесят = 80,
+            девяносто = 90,
+            сто = 100,
+            двести = 200,
+            триста = 300,
+            четыреста = 400,
+            пятьсот = 500,
+            шестьсот = 600,
+            семьсот = 700,
+            восемьсот = 800,
+            девятьсот = 900,
+            тысяча = 1000,
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            /////////////////////////////ДЗ 1/////////////////////////////
+            Console.WriteLine("\t\tДЗ 1");
+            Console.WriteLine("Знайти позицію літери в алфавіті та перевести її в інший регістр\n");
+            //////////////////////////////////////////////////////////////
+            //Формируем алфавит
+            char[] Alfavit = new char[116];
+            for (int i = 0; i < Alfavit.Length; i++)
+            {
+                Alfavit[i] = Convert.ToChar(i + 11);
+            }
+            Console.WriteLine("Исходный массив символов:");
+            foreach (var item in Alfavit)
+            {
+                Console.Write(item);
+            }
+            //Выводим только буквы на консоль
+            Console.WriteLine("\n\nТолько буквы:");
+            foreach (var item in Alfavit)
+            {
+                if (char.IsLetter(item))
+                    Console.Write(item);
+            }
+            //Находим букву и меняем регистр с выводом на консоль
+            Console.WriteLine("\nПосле смены регистра:");
+            for (int i = 0; i < Alfavit.Length; i++)
+            {
+                if (char.IsLetter(Alfavit[i]))
+                {
+                    if (char.IsUpper(Alfavit[i]))
+                        Alfavit[i] = char.ToLower(Alfavit[i]);
+                    else
+                        Alfavit[i] = char.ToUpper(Alfavit[i]);
+                    Console.Write(Alfavit[i]);
+                }
+            }
+            //Ожидание "Enter"
+            Console.WriteLine("\n\nДз 2 -> \"Enter\"...");
+            Console.ReadLine();
+            Console.Clear();
+
+            ///////////////////////////ДЗ 2/////////////////////////////
+            Console.WriteLine("\t\tДЗ 2");
+            Console.WriteLine("Розділювач рядка.\r\n" +
+                              "Дана строка та символ, потрібно розділити строку на кілька строк (масив строк) виходячи із заданого символу.\n" +
+                              "Наприклад: строка = \"Лондон, Париж, Рим\", а символ = ','.\n" +
+                              "Результат = string[] { \"Лондон\", \"Париж\", \"Рим\" }.");
+            //////////////////////////////////////////////////////////////
+            string inStr = "Лондон, Париж, Рим";
+            Console.WriteLine("\nВходящая строка:");
+            Console.WriteLine(inStr);
+            string[] splitMassStr = Splitting(inStr);
+            Console.WriteLine("\nВозвращаем массив городов:");
+            foreach(var line in splitMassStr)
+                Console.WriteLine(line);
+            //Ожидание "Enter"
+            Console.WriteLine("\nДз 3 -> \"Enter\"...");
+            Console.ReadLine();
+            Console.Clear();
+
+            /////////////////////////////ДЗ 3/////////////////////////////
+            Console.WriteLine("\t\tДЗ 3");
+            Console.WriteLine("Пошук підстроки у строці.");
+            //////////////////////////////////////////////////////////////
+            int index;
+            inStr = "один два три четыре два пять четыре";
+            string subStr = "два";
+            Console.WriteLine("Входящая строка: " + inStr);
+            Console.WriteLine("Искомая строка:  " + subStr);
+            //index = inStr.IndexOf(subStr);
+            //if (index != -1)
+            //    Console.WriteLine($"Строка \"{inStr.Substring(index, subStr.Length)}\" встречается на {index + 1} символе");
+            for (int i = 0; i < inStr.Length - subStr.Length; i++)
+            {
+                index = i;
+                for (int j = 0; j < subStr.Length; j++)
+                {
+                    if (subStr[j] != inStr[j+i])
+                    {
+                        index = -1;
+                        break;
+                    }                    
+                }
+                if (index >= 0)
+                    Console.WriteLine($"индекс вхождения -> {index}");             
+            }
+            //Ожидание "Enter"
+            Console.WriteLine("\nДз 4 -> \"Enter\"...");
+            Console.ReadLine();
+            Console.Clear();
+
+            ///////////////////////////ДЗ 4/////////////////////////////
+            Console.WriteLine("\t\tДЗ 4");
+            Console.WriteLine("Написати програму, яка виводить число літерами.Приклад: 117 - сто сімнадцять.");
+            //////////////////////////////////////////////////////////////
+            Console.Write("Введите число -1999..1999 -> ");
+            int inVal = Convert.ToInt32(Console.ReadLine());
+            while (!(inVal > -2000 && inVal < 2000))
+            {
+                Console.Clear();
+                Console.WriteLine("Ошибка ввода!");
+                Console.Write("Введите число -1999..1999 -> ");
+                inVal = Convert.ToInt32(Console.ReadLine());
+            }
+            string intStr = IntToStr(Math.Abs(inVal), 0, "");
+            if (inVal < 0)
+                intStr = "минус " + intStr;
+            Console.WriteLine(intStr);
+            //Ожидание "Enter"
+            Console.WriteLine("\nДз 5 -> \"Enter\"...");
+            Console.ReadLine();
+            Console.Clear();
+
+            /////////////////////////////ДЗ 5/////////////////////////////
+            //Поміняти місцями значення двох змінних (типу int) (без використання 3й)
+            Console.WriteLine("\t\tДЗ 5");
+            Console.WriteLine("Поміняти місцями значення двох змінних (типу int) (без використання 3й)");
+            int int1 = 356;
+            int int2 = 587;
+            Console.WriteLine($"int1 -> {int1}");
+            Console.WriteLine($"int2 -> {int2}");
+            int1 += int2;
+            int2 = int1 - int2;
+            int1 -= int2;
+            Console.WriteLine("Фжух!");
+            Console.WriteLine($"int1 -> {int1}");
+            Console.WriteLine($"int2 -> {int2}");
+        }
+    }
+}
---
+++ DZ_2/Program.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace DZ_2
+{
+    internal class Program
+    {
+        //для ДЗ 2
+        static void dz2_function(int inInt, int step = 1)
+        {
+            if (inInt == 1)
+            {
+                Console.WriteLine($"\nШаг -> {step}");
+                Console.WriteLine($"Число = {inInt}");
+                Console.WriteLine("Расчин окончен!");
+            }
+            else if (inInt % 2 == 0)
+            {
+                Console.WriteLine($"\nШаг -> {step}");
+                Console.WriteLine($"Число = {inInt}");
+                dz2_function(inInt/2, ++step);
+            }
+            else
+            {
+                Console.WriteLine($"\nШаг -> {step}");
+                Console.WriteLine($"Число = {inInt}");
+                dz2_function((inInt * 3 + 1)/2, ++step);
+            }
+        }
+        //Для ДЗ 4
+        static string dz4_ranndomChrs(int inInt)
+        {
+            //65..90 97..122
+            Random random = new Random();
+            string outStr = "";
+            int rendomInt;
+            for (int i = 0; i < inInt;)
+            {
+                rendomInt = random.Next(65, 122);
+                if(rendomInt <= 90 || rendomInt >= 97)
+                {
+                    outStr += Convert.ToChar(rendomInt);
+                    i++;
+                }                
+            }
+            return outStr;
+        }
+        //Для ДЗ 6
+        static void dz6_printPool(int [,] inArr)
+        {
+            Console.Clear();
+            for (int i = 0; i < inArr.GetLength(0); i++)
+            {
+                for (int j = 0; j < inArr.GetLength(1); j++)
+                {
+                    if (inArr[i, j] == 10)
+                        Console.Write(Convert.ToChar(2));
+                    else
+                        Console.Write("_");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void dz6_updateNeighbours(int[,] inPool)
+        {
+            for (int i = 0; i < inPool.GetLength(0); i++)
+            {
+                for (int j = 0; j < inPool.GetLength(1); j++)
+                {
+                    if (inPool[i, j] >= 10)
+                    {
+                        try { inPool[i - 1, j - 1] += 1; } catch (Exception) { }
+                        try { inPool[i - 1, j] += 1; } catch (Exception) { }
+                        try { inPool[i - 1, j + 1] += 1; } catch (Exception) { };
+                        try { inPool[i, j - 1] += 1; } catch (Exception) { };
+                        try { inPool[i, j + 1] += 1; } catch (Exception) { };
+                        try { inPool[i + 1, j - 1] += 1; } catch (Exception) { };
+                        try { inPool[i + 1, j] += 1; } catch (Exception) { };
+                        try { inPool[i + 1, j + 1] += 1; } catch (Exception) { };
+                    }
+                }
+            }
+        }
+        static void dz6_updateLife(int[,] inPool)
+        {
+            for (int i = 0; i < inPool.GetLength(0); i++)
+            {
+                for (int j = 0; j < inPool.GetLength(1); j++)
+                {
+                    if (inPool[i,j]==3 || (inPool[i, j] > 11 && inPool[i, j] < 14))
+                        inPool[i, j] = 10;
+                    else
+                        inPool[i, j] = 0;
+                }
+            }
+        }
+        static bool dz6_compareArrays(int[,] array1, int[,] array2)
+        {
+            //if (array1.GetLength(0) != array2.GetLength(0) || array1.GetLength(1) != array2.GetLength(1))
+            //    return false;
+            for (int i = 0; i < array1.GetLength(0); i++)
+            {
+                for (int j = 0; j < array1.GetLength(1); j++)
+                {
+                    if (array1[i, j] != array2[i, j])
+                        return false;
+                }
+            }
+            return true;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            ///////////////////////////////ДЗ 1/////////////////////////////
+            //Console.WriteLine("\t\tДЗ 1");
+            //Console.WriteLine("Реверс строки/масиву. Без додаткового масиву. Складність О(n).");
+            ////////////////////////////////////////////////////////////////
+            //int[] dz1_intMass = { 1, 2, 3, 4, 5, 6 };
+            //int dz1_tempInt;
+            //foreach (var item in dz1_intMass)
+            //    Console.Write(item + ", ");
+            //Console.WriteLine();
+            //for (int j = 0; j < dz1_intMass.Length / 2; j++)
+            //{
+            //    dz1_tempInt = dz1_intMass[j];
+            //    dz1_intMass[j] = dz1_intMass[dz1_intMass.Length - j - 1];
+            //    dz1_intMass[dz1_intMass.Length - j - 1] = dz1_tempInt;
+            //}
+            //foreach (var item in dz1_intMass)
+            //    Console.Write(item + ", ");
+            ////Ожидание "Enter"
+            //Console.WriteLine("\n\nДз 2 -> \"Enter\"...");
+            //Console.ReadLine();
+            //Console.Clear();
+
+            ///////////////////////////////ДЗ 2/////////////////////////////
+            //Console.WriteLine("\t\tДЗ 2");
+            //Console.WriteLine("Перевірка гіпотези Сиракуз: Візьмемо будь-яке натуральне число.\n" +
+            //                  "Якщо воно парне - розділимо його навпіл, якщо непарне - помножимо на 3, додамо 1 і розділимо навпіл.\n" +
+            //                  "Повторимо ці дії із знову отриманим числом. Гіпотеза свідчить,\n" +
+            //                  "незалежно від вибору першого числа рано чи пізно ми отримаємо 1.\n" +
+            //                  "На вхід – число, при кожній зміні – роздрукувати число. Зробити рекурсивно.");
+            ////////////////////////////////////////////////////////////////            
+            //Console.Write("Введите целое число-> ");
+            //int dz2_inVal = Convert.ToInt32(Console.ReadLine());
+            //dz2_function(dz2_inVal);
+            ////Ожидание "Enter"
+            //Console.WriteLine("\n\nДз 3 -> \"Enter\"...");
+            //Console.ReadLine();
+            //Console.Clear();
+
+            ///////////////////////////////ДЗ 3/////////////////////////////
+            //Console.WriteLine("\t\tДЗ 3");
+            //Console.WriteLine("Фільтрування неприпустимих слів у строці. Має бути саме слова, а не частини слів.");
+            ////////////////////////////////////////////////////////////////
+            //string dz3_inStr = "Текст с повторяющимся словом \"текст\". В этом тексте должно исчезнуть слово текст";
+            //string dz3_keyWord = "текст";
+            //Console.WriteLine("Недопустимое слово: \"" + dz3_keyWord + "\"");
+            //Console.WriteLine(dz3_inStr);
+            //string dz3_regexStr = @"\b" + dz3_keyWord + @"\b";
+            //MatchCollection dz_match = Regex.Matches(dz3_inStr, dz3_regexStr, RegexOptions.IgnoreCase);
+            //foreach (Match item in dz_match)
+            //{
+            //    for (int j = 0; j < dz3_keyWord.Length; j++)
+            //    {
+            //        dz3_inStr = dz3_inStr.Remove(j + item.Index, 1).Insert(j + item.Index, "*");
+            //    }
+            //}
+            //Console.WriteLine(dz3_inStr);
+            ////Ожидание "Enter"
+            //Console.WriteLine("\nДз 3 -> \"Enter\"...");
+            //Console.ReadLine();
+            //Console.Clear();
+
+            ///////////////////////////////ДЗ 4/////////////////////////////
+            //Console.WriteLine("\t\tДЗ 4");
+            //Console.WriteLine("Генератор випадкових символів. На вхід у символів, на виході рядок з випадковими символами.");
+            ////////////////////////////////////////////////////////////////
+            //Console.WriteLine(dz4_ranndomChrs(100));
+
+            ///////////////////////////////ДЗ 5/////////////////////////////
+            //Console.WriteLine("\t\tДЗ 5");
+            //Console.WriteLine("\"Дірка\"(пропущене число) у масиві.");
+            ////////////////////////////////////////////////////////////////
+            //int[] dz5_array = { 1, 2, 3, 4, 6, 7, 8, 9, 10};
+            //int dz5_expectedSum = (dz5_array.Length + 1)*(dz5_array.Length + 2) / 2 ;
+            //int dz5_realSum = 0;
+            //for (int i = 0; i < dz5_array.Length; i++)
+            //{
+            //    dz5_realSum += dz5_array[i];
+            //    Console.Write(dz5_array[i] + " ");
+            //}
+            //Console.WriteLine($"\nПропущено число {dz5_expectedSum - dz5_realSum}");
+
+            /////////////////////////////////ДЗ 6/////////////////////////////
+            //Console.WriteLine("\t\tДЗ 6");
+            //Console.WriteLine("Игра \"Жизнь\":");
+            //////////////////////////////////////////////////////////////////
+            //int[,] dz6_pool = new int[20, 40];
+            //int[,] dz6_previousPool = new int[20, 40];
+            //int[,] dz6_previous2Pool = new int[20, 40];
+            //int[,] dz6_nullPool = new int[20, 40];
+
+            //dz6_pool[0, 1] = 10;
+            //dz6_pool[1, 2] = 10;
+            //dz6_pool[2, 0] = 10;
+            //dz6_pool[2, 1] = 10;
+            //dz6_pool[2, 2] = 10;
+            //dz6_pool[0, 5] = 10;
+            //dz6_pool[1, 5] = 10;
+            //dz6_pool[2, 5] = 10;
+            //while (true)
+            //{
+            //    dz6_previousPool = dz6_pool.Clone() as int[,];
+            //    dz6_printPool(dz6_pool);
+            //    dz6_updateNeighbours(dz6_pool);
+            //    dz6_updateLife(dz6_pool);
+            //    Thread.Sleep(30);
+            //    if (dz6_compareArrays(dz6_pool, dz6_previousPool) || dz6_compareArrays(dz6_pool, dz6_previous2Pool))
+            //        break;
+            //    if (dz6_compareArrays(dz6_pool, dz6_nullPool))
+            //    {
+            //        dz6_printPool(dz6_pool);
+            //        break;
+            //    }
+            //    dz6_previous2Pool = dz6_previousPool.Clone() as int[,];
+            //}
+            //Console.WriteLine("Игра окончена");
+            ////Ожидание "Enter"
+            //Console.WriteLine("\nДз 7 -> \"Enter\"...");
+            //Console.ReadLine();
+            //Console.Clear();
+
+            /////////////////////////////////ДЗ 7/////////////////////////////
+            //Console.WriteLine("\t\tДЗ 7");
+            //Console.WriteLine("Найпростіше стиснення ланцюжка ДНК. Ланцюг ДНК у вигляді строки на вхід(кожен нуклеотид\n" +
+            //    "представлений символом \"A\", \"C\", \"G\", \"T\").Два методи, один для компресії, інший для декомпресії.");
+            //////////////////////////////////////////////////////////////////
+            //string dz7_DNK = "ACTGGACCTCACGGAATCGATACAGATTACACCCATAAGCCTTG";
+            ////Архивация ДНК
+            //string dz7_archiveDNK = "";
+            //int dz7_code = 0;
+            //for (int i = 0; i < dz7_DNK.Length; i++)
+            //{
+            //    switch (dz7_DNK[i])
+            //    {
+            //        case 'A':
+            //            dz7_code = dz7_code + (0 << i%8*2);
+            //            break;
+            //        case 'C':
+            //            dz7_code = dz7_code + (1 << i%8*2);
+            //            break;
+            //        case 'T':
+            //            dz7_code = dz7_code + (2 << i%8*2);
+            //            break;
+            //        case 'G':
+            //            dz7_code = dz7_code + (3 << i%8*2);
+            //            break;
+            //    }
+            //    if ((i + 1) % 8 == 0 || i+1 == dz7_DNK.Length)
+            //    {
+            //        dz7_archiveDNK += Convert.ToChar(dz7_code);
+            //        dz7_code = 0;
+            //        if (i + 1 == dz7_DNK.Length)
+            //        {
+            //            dz7_archiveDNK += Convert.ToChar(dz7_DNK.Length);
+            //        }
+            //    }
+            //}
+            ////Занимаемая память = длина строки * 2 байта + 2 байта на символ конца строки
+            //int dz7_sizeDNK =  dz7_DNK.Length * sizeof(char) + 2;
+            //int dz7_sizeArchiveDNK = dz7_archiveDNK.Length * sizeof(char) + 2;
+            //Console.WriteLine();
+            //Console.Write(dz7_DNK);
+            //Console.WriteLine($" -> {dz7_sizeDNK} байт");
+            //Console.Write(dz7_archiveDNK);
+            //Console.WriteLine($" -> {dz7_sizeArchiveDNK} байт");
+            //Console.WriteLine($"Процент сжатия -> {100 - dz7_sizeArchiveDNK * 100 / dz7_sizeDNK}%");
+            ////Восстановление ДНК
+            //dz7_DNK = "";
+            //for (int i = 0; i < dz7_archiveDNK.Length - 1; i++)
+            //{
+            //    dz7_code = dz7_archiveDNK[i];
+
+            //    for (int j = 0; j < 8; j++)
+            //    {
+            //        dz7_code = dz7_code & 3;
+            //        switch (dz7_code)
+            //        {
+            //            case 0:
+            //                dz7_DNK += 'A';
+            //                break;
+            //            case 1:
+            //                dz7_DNK += 'C';
+            //                break;
+            //            case 2:
+            //                dz7_DNK += 'T';
+            //                break;
+            //            case 3:
+            //                dz7_DNK += 'G';
+            //                break;
+            //        }
+            //        if (dz7_DNK.Length == dz7_archiveDNK[dz7_archiveDNK.Length - 1])
+            //        {
+            //            break;
+            //        }
+            //        dz7_code = dz7_archiveDNK[i] >> (j + 1) * 2;
+            //    }
+            //}
+            //Console.WriteLine(dz7_DNK);
+            ////Ожидание "Enter"
+            //Console.WriteLine("\nДз 8 -> \"Enter\"...");
+            //Console.ReadLine();
+            //Console.Clear();
+        }
+    }
+}
