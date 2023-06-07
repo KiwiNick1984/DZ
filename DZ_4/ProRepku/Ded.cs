2379967f1567fe10f72b90d35647297f64473628
Kiwi <kiwi181284@gmail.com>
2023-06-07 18:44:50 +0300

    Repka

--- DZ_4/ProRepku/Ded.cs
+++ DZ_4/ProRepku/Ded.cs
@@ -33,7 +33,6 @@ namespace DZ_4
                 garden.Add(new Repka(40));
             Console.WriteLine($"Репка посажена на участке #{i + 1}");
         }
-
         public void BoostTheRepka()         //Удобрить репку
         {
             int gardenArea = SelectGardenArea();
@@ -45,9 +44,10 @@ namespace DZ_4
             int gardenArea = SelectGardenArea();
             if (gardenArea > 0)
             {
-                Console.WriteLine(garden[gardenArea - 1].ToPull(dedPower));
+                if (garden[gardenArea - 1].ToPull(dedPower))
+                    garden[gardenArea - 1] = null;
             }
-                
+
             //bool thereIsRepka = false;
 
             //Console.Write("Номера участков с репкой:");
@@ -57,32 +57,26 @@ namespace DZ_4
             //}
             //Console.Write("\nИз какого участка тянуть? -> ");
         }
-
         private int SelectGardenArea()      //Выбор участка с репкой
         {
             int gardenArea = 0;
             bool thereIsRepka = false;
-            while (true)
-            {
+            while (true) {
                 Console.Clear();
                 Console.Write("Номера участков с репкой:");
-                for (int i = 0; i < garden.Count; i++)
-                {
-                    if (garden[i] != null)
-                    {
+                for (int i = 0; i < garden.Count; i++) {
+                    if (garden[i] != null) {
                         Console.Write($" {i + 1},");
                         thereIsRepka = true;
                     }
                 }
-                if (!thereIsRepka)
-                {
+                if (!thereIsRepka) {
                     Console.WriteLine("\nДед или забы посадить репку, или выдернул все!");
                     return 0;
                 }
                 Console.Write("\nВыбери участок -> ");
                 gardenArea = Convert.ToInt32(Console.ReadLine());
-                if (gardenArea > 0 && gardenArea <= garden.Count)
-                {
+                if (gardenArea > 0 && gardenArea <= garden.Count) {
                     if (garden[gardenArea - 1] != null)
                         return gardenArea;
                 }
---
+++ DZ_4/ProRepku/Garden.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_4
+{
+    internal class Garden
+    {
+        List<Repka> garden = new List<Repka>();
+        public void PlantRepka(int powr = 30) {
+            int i;
+            for (i = 0; i < garden.Count; i++) {
+                garden[i] = garden[i] ?? new Repka(powr);
+            }
+            if (i == garden.Count) {
+                garden.Add(new Repka(powr));
+            }
+            Console.WriteLine($"Репка посажена на участке #{i + 1}");
+        }
+        public bool CheсkGardenArea(int gardenArea) {
+            return (gardenArea > 0 && gardenArea <= garden.Count && garden[gardenArea - 1] != null);
+        }
+        public void PrintGarden() {
+            if()
+            {
+
+
+            }
+        }
+    }
+}
