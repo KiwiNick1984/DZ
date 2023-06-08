e51fdb1703aa58943f0477686fe41368aafc8f72
Kiwi <kiwi181284@gmail.com>
2023-06-09 00:48:09 +0300

    Все фигня давай по новой.

---
+++ DZ_4/ProRepku/Plants/Repka.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DZ_4
+{
+    internal class Repka : Plant
+    {
+        public Repka(int repkaPower = 30)  : base(repkaPower)
+        {
+            Console.WriteLine($"Создана репка. Мощь -> {_plantPower}");
+        }
+        public bool Pull(int pullPower)   //Тянуть репку (true-вытащена / false-продолжает расти)
+        {
+            if (_plantPower <= pullPower) {
+                Console.WriteLine("Репка поддалась!");
+                return true;
+            } else {
+                Console.WriteLine("Сила репки кажется безграничной. Нужна помощь");
+                return false;
+            }
+        }
+    }
+}
