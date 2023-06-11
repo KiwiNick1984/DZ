56965a6a5d46513fe961a28f52e19d16e0e406b3
Kiwi <kiwi181284@gmail.com>
2023-06-11 20:00:26 +0300

    add Bank

---
+++ DZ_4/Bank/Client.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_4
+{
+    internal class Client
+    {
+        static int _clientStaticID;     //Для автоматического приращения индекса
+        public int _clientID;           //ID клиента
+        public string _firstName;       //Имя
+        public string _lastName;        //Фамилия
+        public List<Count> _counts = new List<Count>();//Список счетов
+
+        public Client(string firstName, string lastName)
+        {
+            _clientID = ++_clientStaticID;
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+        public void NewCount(Count count) //Добавить счет
+        {
+            _counts.Add(count);
+        }
+        public void GetBalance() //Получить баланс
+        {
+            if (_counts.Count > 0)
+            {
+                foreach (var count in _counts)
+                {
+                    Console.Write("Номер счета: " + count._countNumber + ". Баланс: ");
+                    count._balance.Print();
+                }
+            } else {
+                Console.WriteLine("У клиента нет счета!");
+            }
+
+        }
+    }
+}
