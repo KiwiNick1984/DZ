df16328d7f1b3e5af45deead517a28082aecf077
kiwinick1984 <kiwi181284@gmail.com>
2023-06-16 16:54:15 +0300

    Test IEnumerable

---
+++ Test/Program.cs
@@ -0,0 +1,12 @@
+﻿//List<int> testList = new List<int> { 1, 2, 3 };
+using Test;
+int[] arr = { 2, 67, 99, 100, 200 };
+testEnum ee = new testEnum(arr);
+
+
+foreach (var item in ee)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("Hello, World!");
---
+++ Test/testClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class testEnum : IEnumerable, IEnumerator
+    {
+        private int[] Arr;
+        int position;        
+        public testEnum(int[] inArr)
+        {
+            Arr = inArr;
+            position = -1;
+        }
+        public void Reset() => position = -1;
+        public bool MoveNext() => ++position < Arr.Length;
+        IEnumerator IEnumerable.GetEnumerator() => this;
+
+        public object Current
+        {
+            get {
+                if (position == -1 || position >= Arr.Length)
+                    throw new ArgumentException(); 
+                return Arr[position]; 
+            }
+        }
+    }
+}
