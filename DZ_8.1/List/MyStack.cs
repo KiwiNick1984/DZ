40cedee7f27b9fc9ef895fc26ea9c77cdf0e5597
kiwinick1984 <kiwi181284@gmail.com>
2023-06-23 15:27:47 +0300

    23.06.23 15:27

---
+++ DZ_8.1/List/MyStack.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_8
+{
+    internal class MyStack : IMyCollection
+    {
+        MyList _items = new MyList();
+        public int Count => _items.Count;
+
+        public void Push(object inObj)
+        {
+            _items.Add(inObj);
+        }
+        public object Pop()
+        {
+            object tempObj = _items[Count - 1];
+            _items.RemoveAt(Count - 1);
+            return tempObj;
+        }
+        public object Peek()
+        {
+            return _items[Count - 1];
+        }
+        public object this[int index]
+        {
+            get
+            { return _items[index]; }
+            set
+            { _items[index] = value; }
+        }
+        public bool Contains(object inObj)
+        {
+            return _items.Contains(inObj);
+        }
+        public object[] ToArray()
+        {
+            return _items.ToArray();
+        }
+        public void Clear()
+        {
+            _items.Clear();
+        }
+
+        public IMyEnumerator GetEnumerator() => new Enumerator(this);
+
+        public class Enumerator : IMyEnumerator
+        {
+            private readonly MyStack _list;
+            private int _index;
+            private object? _current;
+
+            public Enumerator(MyStack list)
+            {
+                _list = list;
+                _index = 0;
+                _current = default(object);
+            }
+
+            public object Current => _current;
+
+            public bool MoveNext()
+            {
+                if (_index < _list.Count)
+                {
+                    _current = _list[_index++];
+                    return true;
+                }
+                return false;
+            }
+            public void Reset()
+            {
+                _index = 0;
+                _current = default;
+            }
+        }
+    }
+}
---
+++ DZ_8.1/List/OneWayList.cs
@@ -0,0 +1,178 @@
+﻿namespace DZ_8
+{ 
+    internal class OneWayList : IMyCollection
+    {
+        protected OneWayNode _head;
+        protected OneWayNode _tail;
+        protected int _count;
+
+        public int Count => _count;
+        public object First => _head;
+        public object Last => _tail;
+
+        public OneWayList()
+        {
+            _head = null;
+            _tail = null;
+            _count = 0;
+        }
+
+        public void Add(object inObj)
+        {
+            AddLast(inObj);
+        }
+        public virtual void AddFirst(object data)
+        {
+            _head = CreateNode(data: data, next: _head);
+            _count++;
+        }
+        public virtual void AddLast(object data)
+        {
+            if (_head == null)
+            {
+                _head = CreateNode(data: data);
+                _tail = _head;
+            }
+            else 
+            {
+                OneWayNode current = _head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                _tail = CreateNode(data: data, prev: current);
+            }
+            _count++;
+        }
+        public void Insert(int index, object data)
+        {
+            if ((uint)index > (uint)_count)
+            {
+                throw new Exception("ОШИБКА! Insert выход за пределы диапазона!");
+            }
+
+            if (index == _count)
+            {
+                AddLast(data);
+            }
+            else if (index == 0)
+            {
+                AddFirst(data);
+            }
+            else
+            {
+                OneWayNode current = _head;
+                for (int i = 0; i < _count; i++)
+                {
+                    if (i == index-1)
+                    {
+                        CreateNode(data: data, next: current.Next, prev: current);
+                        break;
+                    }
+                    current = current.Next;
+                }
+            }
+            _count++;
+        }
+        public void Clear()
+        {
+            OneWayNode currentNode = _head;
+            while (currentNode != null)
+            {
+                OneWayNode tempNode = currentNode;
+                currentNode = currentNode.Next;
+                DeleteNode(tempNode);
+            }
+            _head = null;
+            _tail = null;
+            _count = 0;
+        }
+        public bool Contains(object inObj)
+        {
+            OneWayNode current = _head;
+            while (current?.Data != null)
+            {
+                if (current.Data.Equals(inObj))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+        public object[] ToArray()
+        {
+            object[] array = new object[_count];
+            OneWayNode current = _head;
+            for (int i = 0; current?.Data != null; i++)
+            {
+                array[i] = current.Data;
+                current = current.Next;
+            }
+            return array;
+        }
+        protected virtual OneWayNode CreateNode(object data, OneWayNode next = null, OneWayNode prev = null)
+        {
+            OneWayNode newDode = new OneWayNode(data, next);
+            if (prev != null)
+                prev.Next = newDode;
+            return newDode;
+        }
+        protected virtual void DeleteNode(OneWayNode current, OneWayNode next = null, OneWayNode prev = null)
+        {
+            current.Next = null;
+            current.Data = null;
+        }
+
+        public IMyEnumerator GetEnumerator() => new Enumerator(this);
+        public class Enumerator : IMyEnumerator
+        {
+            private OneWayNode _currentNode;
+            private object? _currentData;
+
+            public Enumerator(OneWayList list)
+            {
+                _currentNode = list._head;
+            }
+
+            public object Current => _currentData;
+
+            public bool MoveNext()
+            {
+                if(_currentNode?.Data != null)
+                {
+                    _currentData = _currentNode.Data;
+                    if (_currentNode?.Next != null)
+                        _currentNode = (OneWayNode)_currentNode.Next;
+                    else
+
+                        _currentNode = null;
+                    return true;
+                }
+                return false;
+            }
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+    class OneWayNode
+    {
+        private OneWayNode _next;
+        private Object _data;
+        public OneWayNode Next
+        {
+            get { return _next; }
+            set { _next = value; }
+        }        
+        public Object Data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+        public OneWayNode(object inObjData, OneWayNode next = null)
+        {
+            _data = inObjData;
+            _next = next;
+        }        
+    }
+}
