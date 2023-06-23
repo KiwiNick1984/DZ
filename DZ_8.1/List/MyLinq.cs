3d4a658afc30f0df8c8b0ccbc0fabc1f67a7e055
kiwinick1984 <kiwi181284@gmail.com>
2023-06-23 15:27:47 +0300

    23.06.23 15:27

---
+++ DZ_8.1/List/MyLinq.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DZ_8
+{
+    internal static class Mylinq
+    {
+        class MyLinq_CreateEnumerator : IMyEnumerable
+        {
+            private readonly Func<IMyEnumerator> _createEnumerator;
+            public MyLinq_CreateEnumerator(Func<IMyEnumerator> createEnumerator)
+            {
+                _createEnumerator = createEnumerator;
+            }
+
+            public IMyEnumerator GetEnumerator()
+            {
+                return _createEnumerator();
+            }
+        }
+        class MyWhereEnumerator : IMyEnumerator
+        {
+            public readonly IMyEnumerator _enumerator;
+            private readonly Predicate<object> _predicate;
+            public object Current => _enumerator.Current;
+
+            public MyWhereEnumerator(IMyEnumerable enumerator, Predicate<object> predicate)
+            {
+                _enumerator = enumerator.GetEnumerator();
+                _predicate = predicate;
+            }
+
+            public bool MoveNext()
+            {
+                while (_enumerator.MoveNext())
+                {
+                    if (_predicate(Current))
+                        return true;
+                }
+                return false;
+            }
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        class MySkipEnumerator : IMyEnumerator
+        {
+            public readonly IMyEnumerator _enumerator;
+            public int _count;
+
+            public MySkipEnumerator(IMyEnumerable enumerator, int count)
+            {
+                _enumerator = enumerator.GetEnumerator();
+                _count = count;
+            }
+
+            public object Current => _enumerator.Current;
+            public bool MoveNext()
+            {
+                for (; _count > 0; _count--)
+                    _enumerator.MoveNext();
+                return _enumerator.MoveNext();
+            }
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        class MySkipWhileEnumerator : IMyEnumerator
+        {
+            public readonly IMyEnumerator _enumerator;
+            private readonly Predicate<object> _predicate;
+            bool _noSkip;
+
+            public MySkipWhileEnumerator(IMyEnumerable enumerator, Predicate<object> predicate)
+            {
+                _enumerator = enumerator.GetEnumerator();
+                _predicate = predicate;
+                _noSkip = false;
+            }
+
+            public object Current => _enumerator.Current;
+            public bool MoveNext()
+            {
+                while(_enumerator.MoveNext())
+                {
+                    if(_noSkip || !_predicate(Current))
+                    {
+                        _noSkip = true;
+                        return true;
+                    }                        
+                }
+                return false;
+            }
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        class MyTakeEnumerator : IMyEnumerator
+        {
+            public readonly IMyEnumerator _enumerator;
+            public int _count;
+
+            public MyTakeEnumerator(IMyEnumerable enumerator, int count)
+            {
+                _enumerator = enumerator.GetEnumerator();
+                _count = count;
+            }
+
+            public object Current => _enumerator.Current;
+            public bool MoveNext()
+            {
+                while(_enumerator.MoveNext() && _count>0)
+                {
+                    _count--;
+                    return true;
+                }
+                return false;
+            }
+
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        class MyTakeWhileEnumerator : IMyEnumerator
+        {
+            public readonly IMyEnumerator _enumerator;
+            private readonly Predicate<object> _predicate;
+            bool _take;
+
+            public MyTakeWhileEnumerator(IMyEnumerable enumerator, Predicate<object> predicate)
+            {
+                _enumerator = enumerator.GetEnumerator();
+                _predicate = predicate;
+                _take = true;
+            }
+
+            public object Current => _enumerator.Current;
+            public bool MoveNext()
+            {
+                while (_enumerator.MoveNext() && _take)
+                {
+                    if (_predicate(Current))
+                        return true;
+                    else
+                        _take = false;
+                }
+                return false;
+            }
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        class MyFirstEnumerator : IMyEnumerator
+        {
+            public readonly IMyEnumerator _enumerator;
+            private readonly Predicate<object> _predicate;
+            bool _take;
+
+            public MyFirstEnumerator(IMyEnumerable enumerator, Predicate<object> predicate)
+            {
+                _enumerator = enumerator.GetEnumerator();
+                _predicate = predicate;
+                _take = true;
+            }
+
+            public object Current => _enumerator.Current;
+            public bool MoveNext()
+            {
+                while (_enumerator.MoveNext() && _take)
+                {
+                    if (_predicate == null)
+                    {
+                        _take = false;
+                        return true;
+                    }
+                    if (_predicate(Current))
+                    {
+                        _take = false;
+                        return true;
+                    }
+                }
+                return false;
+            }
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        class MyLastEnumerator : IMyEnumerator
+        {
+            public readonly IMyEnumerator _enumerator;
+            private readonly Predicate<object> _predicate;
+            object _current;
+            bool _take;
+
+            public MyLastEnumerator(IMyEnumerable enumerator, Predicate<object> predicate)
+            {
+                _enumerator = enumerator.GetEnumerator();
+                _predicate = predicate;
+                _current = _enumerator.Current;
+            }
+
+            public object Current => _current;
+            public bool MoveNext()
+            {
+                _take = false;
+                while (_enumerator.MoveNext())
+                {
+                    if (_predicate == null)
+                    {
+                        _current = _enumerator.Current;
+                        _take = true;
+                    }
+                    else if (_predicate(_enumerator.Current))
+                    {
+                        _current = _enumerator.Current;
+                        _take = true;
+                    }
+                }
+                return _take;
+            }
+            public void Reset()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public static IMyEnumerable MyWhere(this IMyEnumerable enumerable, Predicate<object> predicate)
+        {
+            return new MyLinq_CreateEnumerator(() => new MyWhereEnumerator(enumerable, predicate));
+        }
+        public static IMyEnumerable MySkip(this IMyEnumerable enumerable, int count)
+        {
+            return new MyLinq_CreateEnumerator(() => new MySkipEnumerator(enumerable, count));
+        }
+        public static IMyEnumerable MySkipWhile(this IMyEnumerable enumerable, Predicate<object> predicate)
+        {
+            return new MyLinq_CreateEnumerator(() => new MySkipWhileEnumerator(enumerable, predicate));
+        }
+        public static IMyEnumerable MyTake(this IMyEnumerable enumerable, int count)
+        {
+            return new MyLinq_CreateEnumerator(() => new MyTakeEnumerator(enumerable, count));
+        }
+        public static IMyEnumerable MyTakeWhile(this IMyEnumerable enumerable, Predicate<object> predicate)
+        {
+            return new MyLinq_CreateEnumerator(() => new MyTakeWhileEnumerator(enumerable, predicate));
+        }
+        public static IMyEnumerable MyFirst(this IMyEnumerable enumerable, Predicate<object> predicate = null)
+        {
+            return new MyLinq_CreateEnumerator(() => new MyFirstEnumerator(enumerable, predicate));
+        }
+        public static IMyEnumerable MyLast(this IMyEnumerable enumerable, Predicate<object> predicate = null)
+        {
+            return new MyLinq_CreateEnumerator(() => new MyLastEnumerator(enumerable, predicate));
+        }
+        public static IMyEnumerable MySelect(this IMyEnumerable enumerable, Predicate<object> predicate = null)
+        {
+            return new MyLinq_CreateEnumerator(() => new MyLastEnumerator(enumerable, predicate));
+        }
+
+    }
+}
---
+++ DZ_8.1/List/MyList.cs
@@ -0,0 +1,231 @@
+﻿using System;
+
+namespace DZ_8.Generic
+{
+    internal class MyList<T> : IMyList<T>, IMyList, IMyCollection<T>, IMyEnumerable<T>, IMyEnumerable
+    {
+        private T[] _items;
+        private int _size;
+        private int _capacity;
+
+        public int Count => _size;
+        public int Capacity
+        {
+            get => _capacity;
+        }
+
+        public MyList() : this(4) { }
+        public MyList(int inCapacity = 4)
+        {
+            _items = new T[0];
+            EnsureCapacity(inCapacity);
+        }
+
+        public void Add(T inItem)
+        {
+            if (_size == _items.Length)
+            {
+                EnsureCapacity(_size + 1);
+            }
+            _items[_size++] = inItem;
+        }
+        void IMyList.Add(object inItem)
+        {
+            try
+            {
+                Add((T)inItem);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        public T this[int index]
+        {
+            get
+            { return _items[index]; }
+            set
+            { _items[index] = value; }
+        }
+        object IMyList.this[int index]
+        {
+            get
+            { return _items[index]; }
+            set
+            { 
+                try
+                {
+                    _items[index] = (T)value;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }   
+        public void Clear()
+        {
+            for (int i = 0; i < _items.Length; i++)
+            {
+                _items[i] = default(T);
+            }
+            _size = 0;
+        }
+        public bool Contains(object inObj)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                if (inObj.Equals(_items[i]))
+                    return true;
+            }
+            return false;
+        }
+        public void Insert(int index, T inItem)
+        {
+            if ((uint)index > (uint)_size)
+            {
+                throw new Exception("ОШИБКА! выход за пределы диапазона!");
+            }
+            if (_size == _items.Length)
+            {
+                EnsureCapacity(_size + 1);
+            }
+            for (int i = _size; i > index; i--)
+            {
+                _items[i] = _items[i - 1];
+            }
+            _items[index] = inItem;
+            _size++;
+        }
+        void IMyList.Insert(int index, object inIndex)
+        {
+            try
+            {
+                Insert(index, (T)inIndex);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+        public void Remove(object inObj)
+        {
+            int num = IndexOf(inObj);
+            if (num >= 0)
+            {
+                RemoveAt(num);
+            }
+        }
+        public void RemoveAt(int index)
+        {
+            if ((uint)index >= (uint)_size)
+            {
+                throw new Exception("ОШИБКА! Выход за пределы диапазона!");
+            }
+            for (int i = index; i < _size - 1; i++)
+            {
+                _items[i] = _items[i + 1];
+            }
+            _items[_size - 1] = default(object);
+            _size--;
+        }
+        public int IndexOf(T inItem)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                if (inItem.Equals(_items[i]))
+                    return i;
+            }
+            return -1;
+        }
+        int IMyList.IndexOf(object inObj)
+        {
+            return IndexOf((T)inObj);
+        }
+        public T[] ToArray()
+        {
+            T[] array = new T[_size];
+            for (int i = 0; i < _size; i++)
+            {
+                array[i] = _items[i];
+            }
+            return array;
+        }
+        public void Reverse()
+        {
+            object tempObj;
+            for (int i = 0; i < _size / 2; i++)
+            {
+                tempObj = _items[i];
+                _items[i] = _items[_size - i - 1];
+                _items[_size - i - 1] = tempObj;
+            }
+        }
+
+        private void EnsureCapacity(int min)
+        {
+            if (_items.Length < min)
+            {
+                _capacity = ((_items.Length == 0) ? 4 : (_items.Length * 2));
+                if ((uint)_capacity > 2146435071u)
+                {
+                    _capacity = 2146435071;
+                }
+                if (_capacity < min)
+                {
+                    _capacity = min;
+                }
+
+                object[] tempObjArr = new object[_capacity];
+                for (int i = 0; i < _items.Length; i++)
+                {
+                    tempObjArr[i] = _items[i];
+                }
+                _items = tempObjArr;
+            }
+        }
+
+        IMyEnumerator IMyEnumerable.GetEnumerator() => new Enumerator(this);
+
+        IMyEnumerator<T> GetEnumerator() => new Enumerator(this);
+
+        IMyEnumerator<T> IMyEnumerable<T>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public class Enumerator : IMyEnumerator, IMyEnumerator<T>
+        {
+            private readonly MyList<T> _list;
+            private int _index;
+            private T _current;
+
+            public Enumerator(MyList<T> list)
+            {
+                _list = list;
+                _index = 0;
+                _current = default(T);
+            }
+
+            public T Current => _current;
+
+            object IMyEnumerator.Current => _current;
+
+            public bool MoveNext()
+            {
+                if (_index < _list.Count)
+                {
+                    _current = _list[_index++];
+                    return true;
+                }
+                return false;
+            }
+            public void Reset()
+            {
+                _index = 0;
+                _current = default;
+            }
+        }
+    }
+}
