cd8d65d544578767ff8a6e7df0da8a7601bee0a4
kiwinick1984 <kiwi181284@gmail.com>
2023-06-28 17:14:48 +0300

    list.Where

--- DZ_6/Program.cs
+++ DZ_6/Program.cs
@@ -10,6 +10,24 @@ namespace DZ_6
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Unicode;
+            //1. Виведіть усі числа від 10 до 50 через кому
+            Console.WriteLine("Виведіть усі числа від 10 до 50 через кому");
+            foreach (int index in Enumerable.Range(10, 40))
+            {
+                Console.Write(index + ", ");
+            }
+            Console.WriteLine();
+            //2.Виведіть лише ті числа від 10 до 50, які можна поділити на 3
+            Console.WriteLine("Виведіть лише ті числа від 10 до 50, які можна поділити на 3");
+            List<int> list = Enumerable.Range(10, 40).ToList();
+            var enumerable = list.Where(x => (x%3) == 0);
+            foreach (int index in enumerable)
+            {
+                Console.Write(index + ", ");
+            }
+
+
         }
     }
 }
