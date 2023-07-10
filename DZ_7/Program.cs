e59addc4d87262eb9e81c0c2892f8105fd6f63db
Kiwi <kiwi181284@gmail.com>
2023-07-10 08:06:59 +0300

    9.

--- DZ_7/Program.cs
+++ DZ_7/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace DZ_7
 {
-    class Actor
+    public class Actor
     {
         public string Name { get; set; }
         public DateTime Birthdate { get; set; }
@@ -29,14 +33,21 @@ namespace DZ_7
     {
         public int Pages { get; set; }
     }
+
+
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {            
             var data = new List<object>()
             {
                 "Hello",
-                new Book() { Author = "Terry Pratchett", Name = "Guards! Guards!", Pages = 810 },
+                new Book() 
+                {
+                    Author = "Terry Pratchett", 
+                    Name = "Guards! Guards!", 
+                    Pages = 810 
+                },
                 new List<int>() {4, 6, 8, 2},
                 new string[] {"Hello inside array"},
                 new Film()
@@ -48,6 +59,15 @@ namespace DZ_7
                         new Actor() { Name = "Jack Nickolson", Birthdate = new DateTime(1937, 4, 22)},
                         new Actor() { Name = "Leonardo DiCaprio", Birthdate = new DateTime(1974, 11, 11)},
                         new Actor() { Name = "Matt Damon", Birthdate = new DateTime(1970, 8, 10)}
+                    }
+                },
+                new Film()
+                {
+                    Author = "Martin Scorsese",
+                    Name= "Film 2",
+                    Actors = new List<Actor>()
+                    {
+                        new Actor() { Name = "Jack Nickolson", Birthdate = new DateTime(1937, 4, 22)},
                     }
                 },
                 new Film()
@@ -76,19 +96,68 @@ namespace DZ_7
                 },
                 "Leonardo DiCaprio"
             };
-            foreach (var item in data.Where(i => !(i is ArtObject)))
+            //1.Виведіть усі елементи, крім ArtObjects
+            Console.WriteLine("1");
+            Console.WriteLine(string.Join(", ", data.Where(i => !(i is ArtObject))));
+            Console.WriteLine();
+            //2. Виведіть імена всіх акторів
+            Console.WriteLine("2");
+            Console.WriteLine(string.Join(", ", data.OfType<Film>().Cast<Film>().SelectMany(f => f.Actors.Select(a => a?.Name)).Distinct()));
+            Console.WriteLine();
+            //3.Виведіть кількість акторів, які народилися в серпні
+            Console.WriteLine("3");
+            Console.WriteLine(string.Join(", ", data.OfType<Film>().Cast<Film>().SelectMany(i => i.Actors.Where(a => a.Birthdate.Month == 8).Select(a => a.Name)).Distinct()));
+            Console.WriteLine();
+            //4.Виведіть два найстаріших імена акторів
+            Console.WriteLine("4");
+            Console.WriteLine(string.Join(", ", data.OfType<Film>().Cast<Film>().SelectMany(i => i.Actors).OrderBy(a => a.Birthdate).Select(a => a.Name).Take(2)));
+            Console.WriteLine();
+            //5.Вивести кількість книг на авторів
+            Console.WriteLine("5");
+            Console.WriteLine(string.Join(", ", data.OfType<Book>().Cast<Book>().GroupBy(b => b.Author).Select(x => string.Join(" ", x.Key, x.Count()))));
+            Console.WriteLine();
+            //6.Виведіть кількість книг на одного автора та фільмів на одного режисера
+            Console.WriteLine("6");
+            Console.WriteLine(string.Join(", ", data.Where(b => b is Film || b is Book).GroupBy(b => (b is Film) ? ((Film)b).Author : ((Book)b).Author).Select(x => string.Join(" ", x.Key, x.Count()))));
+            Console.WriteLine();
+            //7.Виведіть, скільки різних букв використано в іменах усіх акторів
+            Console.WriteLine("7");
+            Console.WriteLine(string.Join(", ", data.OfType<Film>().Cast<Film>().SelectMany(f => f.Actors.Select(a => a?.Name)).GroupBy(a => a).Select(a => string.Join(" ", a.Key, (a.Key).Replace(" ", "").Distinct().Count()))));
+            Console.WriteLine();
+            //8.Виведіть назви всіх книг, упорядковані за іменами авторів і кількістю сторінок
+            Console.WriteLine("8");
+            Console.WriteLine(string.Join(", ", data.OfType<Book>().Cast<Book>().OrderBy(b => b.Author).ThenBy(b => b.Pages).Select(b => b.Name)));
+            Console.WriteLine();
+            //9.Виведіть ім'я актора та всі фільми за участю цього актора
+
+            var rerult = data.OfType<Film>().
+                  Cast<Film>().
+                  SelectMany(f => f.Actors).
+                  Select(f => f.Name).
+                  Distinct().
+                  Select(an => string.Concat(an + "\n", string.Join("\n", data.OfType<Film>().
+                                                                        Cast<Film>().
+                                                                        Where(f => f.Actors.Any(a => a.Name == an)).
+                                                                        Select(f => "\t" + f.Name)
+                                                            )
+                            
+                                            )
+                        );
+            foreach (var item in rerult)
             {
                 Console.WriteLine(item);
             }
-            //foreach (var item in data.Select(c =>
-            //    {
-            //        if (c is Film ArtObject)
-            //            return null;
-            //        return c; 
-            //    }).SelectMany(c => c.object))
+
+            //10.Виведіть суму загальної кількості сторінок у всіх книгах і всі значення int у всіх послідовностях у даних
+            //Console.WriteLine(10);
+            //foreach (var item in data.Where(i => i is List<int> || i is Book).Select(i => (i is Book) ? ((Book)i).Pages:1))
             //{
             //    Console.WriteLine(item);
             //}
+            //11.Отримати словник з ключем - автор книги, значенням - список авторських книг
+            //Dictionary<string, List<Book>> dict = data.OfType<Book>().Cast<Book>().GroupBy(x => x.Author, )
+            //12.Вивести всі фільми "Метт Деймон", за винятком фільмів з акторами, імена яких представлені в даних у вигляді рядків
+
         }
     }
 }
