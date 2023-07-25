6754bbad531a76a6390b4ff7449430c9d9b463d0
Kiwi <kiwi181284@gmail.com>
2023-07-25 23:17:31 +0300

    DZ_9 finish

---
+++ DZ_9/DriverNav.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DZ_9
+{
+    internal class DriverNav
+    {
+        int _navIndex;
+        int _count;
+        DriveInfo[] _drivers;
+        DirectoryInfo _dirInfo;
+        DirectoryInfo[] _dirs;
+        FileInfo[] _files;
+        string _path;
+
+        public DriverNav()
+        {
+            _navIndex = 0;
+            _drivers = DriveInfo.GetDrives();
+            _count = _drivers.Length;
+            _path = _drivers[_navIndex].Name;
+            Print();
+        }
+
+        void Print()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            if (_dirInfo == null ) 
+            {
+                for (int i = 0; i < _count; i++)
+                {
+                    if (i == _navIndex)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    Console.WriteLine($"  {_drivers[i]}" + new string(' ', 8 - _drivers[i].Name.Length));
+                }
+            }
+            else
+            {
+                int strLength;
+                string strName;
+                Console.WriteLine($"===={_path}" + new string('=', 35 - _path.Length));
+                for (int i = 0; i < _count; i++)
+                {
+                    if (i == _navIndex)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Gray;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    if(i < _dirs.Length)
+                    {
+                        strLength = _dirs[i].Name.Length < 28 ? _dirs[i].Name.Length : 28;
+                        strName = _dirs[i].Name.Length < 28 ? _dirs[i].Name : _dirs[i].Name .Substring(0, 25) + "...";
+                        Console.WriteLine($"  {strName}" + new string(' ', 35 - strLength));
+                    }                        
+                    else
+                    {
+                        strLength = _files[i - _dirs.Length].Name.Length < 28 ? _files[i - _dirs.Length].Name.Length : 28;
+                        strName = _files[i - _dirs.Length].Name.Length < 28 ? _files[i - _dirs.Length].Name : _files[i - _dirs.Length].Name.Substring(0, 25) + "...";
+                        Console.WriteLine($"  {strName}" + new string(' ', 30 - strLength) + _files[i - _dirs.Length].Extension + ' ');
+                    }
+
+                        
+                }
+            }
+        }
+
+        public void Up()
+        { 
+            if(_navIndex == 0)
+                _navIndex = _count-1;
+            else
+                _navIndex--;
+            Print();
+        }
+        public void Down()
+        {
+            if (_navIndex == _count - 1)
+                _navIndex = 0;
+            else
+                _navIndex++;
+            Print();
+        }
+        public void Select()
+        {
+            if (_dirInfo == null)
+            {
+                _dirInfo = new DirectoryInfo(_drivers[_navIndex].Name);
+                _path = _dirInfo.FullName;
+            }
+            else
+            {
+                if(_navIndex < _dirs.Length)
+                {
+                    _dirInfo = new DirectoryInfo(_dirs[_navIndex].FullName);
+                    _path = _dirInfo.FullName;
+                }
+                else
+                {
+                    Console.WriteLine(_files[_navIndex - _dirs.Length].FullName);
+                    Console.Clear();                    
+                    PrintTxt();
+                }
+            }
+            _dirs = _dirInfo.GetDirectories();
+            _files = _dirInfo.GetFiles();
+            _count = _dirs.Length + _files.Length;
+            _navIndex = 0;
+            Print();
+        }
+        public void Back()
+        {            
+            if (Directory.GetParent(_path) == null)
+            {
+                _dirInfo = null;
+                _path = _drivers[_navIndex].Name;
+                _count = _drivers.Length;
+            }
+            else
+            {
+                _dirInfo = new DirectoryInfo(Directory.GetParent(_path).FullName);
+                _path = _dirInfo.FullName;
+                _dirs = _dirInfo.GetDirectories();
+                _files = _dirInfo.GetFiles();
+                _count = _dirs.Length + _files.Length;
+                _navIndex = 0;
+            }
+            Print();
+        }
+
+        private void PrintTxt()
+        {            
+            String line;
+            if (_files[_navIndex - _dirs.Length].Extension == ".txt") 
+            {
+                try
+                {
+                    StreamReader sr = new StreamReader(_files[_navIndex - _dirs.Length].FullName);
+                    line = sr.ReadLine();
+                    while (line != null)
+                    {
+                        Console.WriteLine(line);
+                        line = sr.ReadLine();
+                    }
+                    sr.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("КОНЕЦ ФАЙЛА");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Этот файл не txt");
+            }
+            Console.WriteLine("->>Нажмите Enter для выхода");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DZ_9/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DZ_9
+{
+    internal class Program
+    {
+        //Открывает только txt файлы
+        //Стрелка вверх/вниз    - Навигация
+        //Enter                 - Выбор/чтение txt
+        //Backspace             - Назад
+        //Escape                - Выход
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo cki = new ConsoleKeyInfo();
+
+            DriverNav driverNav = new DriverNav();
+
+            while (cki.Key != ConsoleKey.Escape)
+            {
+                cki = Console.ReadKey();
+                switch (cki.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        driverNav.Up();
+                        break;
+                    case ConsoleKey.DownArrow:
+                        driverNav.Down();
+                        break;
+                    case ConsoleKey.Enter:
+                        driverNav.Select();
+                        break;
+                    case ConsoleKey.Backspace:
+                        driverNav.Back();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
