2bc0f25c7437fed16dfa58edf6559c296ed0b9f6
Kiwi <kiwi181284@gmail.com>
2023-07-26 23:26:07 +0300

    DZ_10 start

---
+++ DZ_10/List_Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DZ_5.Generic;
+
+namespace DZ_10
+{
+    [TestClass]
+    public class List_Tests
+    {
+        [TestMethod]
+        [TestCategory("MyList")]
+        public void List_Tests_Add()
+        {
+            MyList<int> myList = new MyList<int>();
+            myList.Add(0);
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+            myList.Add(5);
+            for (int i = 0; i < myList.Count; i++) 
+            {
+                Assert.AreEqual(i, myList[i]);
+            }
+        }
+    }
+}
---
+++ DZ_10/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DZ_10
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
