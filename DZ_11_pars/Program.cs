8924949c68dc4934839dece98c42218120d503b6
Kiwi <kiwi181284@gmail.com>
2023-08-05 20:21:33 +0300

    DZ_11_finish_2

--- DZ_11_pars/Program.cs
+++ DZ_11_pars/Program.cs
@@ -1,38 +1,100 @@
-﻿using System.Text.Json;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.Json;
 
 internal class Program
 {
-    record CityInfo(string cityName, double square, int population, string countryRegion);
+    record CityInfo(string cityName, double square, int population, string country, string district);
+
     private static void Main(string[] args)
     {
-        string pathCityInfo;
-        if (args.Length == 0)
-            pathCityInfo = Path.GetTempPath() + "CityInfo_List.txt";
-        else
-            pathCityInfo = args[0];
-
-        string strForReadFile;
-        string[] str;
-        List<CityInfo> _cityInfo = new List<CityInfo>();
-        using (StreamReader sr = new StreamReader(pathCityInfo))
-        {
-            strForReadFile = sr.ReadLine();
-
-            while (strForReadFile != null)
-            {
-                str = strForReadFile.Split(';');
-
-                _cityInfo.Add(new CityInfo(str[0], double.Parse(str[1]), int.Parse(str[2]), str[3]));
-                strForReadFile = sr.ReadLine();
-            }
-        }
-        using (FileStream fs = new FileStream(Path.GetTempPath() + "CityInfo_List.json", FileMode.OpenOrCreate))
-        {
-            foreach (var item in _cityInfo)
-            {
-                JsonSerializer.Serialize<CityInfo>(fs, item);
-            }            
-        }
+        CityParser cityParser = new CityParser();
+        cityParser.SimpleParser();
+        cityParser.SpanParser();
     }
+    public class CityParser
+    {
+        List<CityInfo> cityInfoList = new List<CityInfo>();
 
+        public void SimpleParser()
+        {
+            string[] splitedLine;
+
+            string cityName;
+            double square;
+            int population;
+            string coutri;
+            string district;
+
+            cityInfoList.Clear();
+            var sw = Stopwatch.StartNew();
+            foreach (var line in File.ReadLines("../../../../CityInfo.txt"))
+            {
+                splitedLine = line.Split(':');
+                cityName = splitedLine[0];
+
+                splitedLine = splitedLine[1].Split(";");
+                square = double.Parse(splitedLine[0]);
+                population = int.Parse(splitedLine[1]);
+
+                splitedLine = splitedLine[2].Split('(', ')');
+                coutri = splitedLine[0];
+                district = splitedLine[1];
+                cityInfoList.Add(new CityInfo(cityName, square, population, coutri, district));
+            }
+            sw.Stop();
+            Console.WriteLine($"SimpleParser: {sw.Elapsed}");
+        }
+        public void SpanParser()
+        {
+            ReadOnlySpan<char> lineSpan;
+
+            string cityName;
+            double square;
+            int population;
+            string coutri;
+            string district;
+
+            cityInfoList.Clear();
+            var sw = Stopwatch.StartNew();
+            foreach (var line in File.ReadLines("../../../../CityInfo.txt"))
+            {
+                lineSpan = line.AsSpan();
+
+                cityName = lineSpan.Slice(0, line.IndexOf(':')).ToString();
+                lineSpan = lineSpan.Slice(line.IndexOf(':') + 1);
+
+                square = double.Parse(lineSpan.Slice(0, lineSpan.IndexOf(";")));
+                lineSpan = lineSpan.Slice(lineSpan.IndexOf(';') + 1);
+
+                population = int.Parse(lineSpan.Slice(0, lineSpan.IndexOf(';')));
+                lineSpan = lineSpan.Slice(lineSpan.IndexOf(';') + 1);
+
+                coutri = lineSpan.Slice(0, lineSpan.IndexOf("(")).ToString();
+                lineSpan = lineSpan.Slice(lineSpan.IndexOf('(') + 1);
+
+                district = lineSpan[..^1].ToString();
+
+
+                cityInfoList.Add(new CityInfo(cityName, square, population, coutri, district));
+            }
+            sw.Stop();
+            Console.WriteLine($"SpanParser: {sw.Elapsed}");
+
+            using (FileStream fs = new FileStream("../../../../CityInfo.json", FileMode.OpenOrCreate))
+            {
+                foreach (var line in cityInfoList)
+                {
+                    JsonSerializer.Serialize<CityInfo>(fs, line);
+                }
+            }
+            //{
+            //    foreach (var line in _cityInfo)
+            //    {
+            //        JsonSerializer.Serialize<CityInfo>(fs, line);
+            //    }            
+            //}
+        }
+        //City_3:265,79;4942;Countri_1(District_5)
+    }
 }
