f759131eee7e90c4d75c8bbc9973485f41787054
Kiwi <kiwi181284@gmail.com>
2023-08-06 12:37:55 +0300

    DZ_12_Start

---
+++ DZ_12/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Diod diod = new Diod(ConsoleColor.Green, 1, 1, 500, 300);
+    }
+}
+
+class Diod
+{
+    private ConsoleColor _color;
+    private int _x;
+    private int _y;
+    private int _onTime;
+    private int _offTime;
+
+    public Diod(ConsoleColor color, int x, int y, int onTime, int offTime)
+    {
+        _color = color;
+        _x = x;
+        _y = y;
+        _offTime = onTime;
+        _offTime = offTime;
+    }
+}
